823909784b4937a36eab23db1727ae7613b8bd7c
Renan Toniolo Rocha <renantoniolo@gmail.com>
2025-09-15 20:05:09 -0300

    Initial Commit

---
+++ guidedBreathingApp Watch App/guidedBreathingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  guidedBreathingAppApp.swift
+//  guidedBreathingApp Watch App
+//
+//  Created by Renan Toniolo Rocha on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct guidedBreathingApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ guidedBreathingApp/guidedBreathingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  guidedBreathingAppApp.swift
+//  guidedBreathingApp
+//
+//  Created by Renan Toniolo Rocha on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct guidedBreathingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
